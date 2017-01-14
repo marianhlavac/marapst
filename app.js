e9a3ca0826a40b7b6d5d9fa91a300de86c221b47
Marián Hlaváč <hlavac95@gmail.com>
2017-01-14 17:22:51 +0100

    Display tags

--- app.js
+++ app.js
@@ -5,9 +5,14 @@ var store = {
 function updateUI() {
   question = store.questions[store.displayedQuestion]
 
-  $("#question-id").html(store.displayedQuestion + 1)
-  $("#question-content").html(question.question)
-  $("#question-solution").html(question.solution)
+  $('#question-id').html(store.displayedQuestion + 1)
+  $('#question-content').html(question.question.replace(/\n/g,'<br>'))
+  $('#question-solution').html(question.solution.replace(/\n/g,'<br>'))
+
+  $('#question-tags').html('')
+  question.tags.map(function(tag) {
+    $('#question-tags').append(`<span class="uk-badge">${tag}</span>`)
+  })
 }
 
 // ---
