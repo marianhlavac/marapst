e0b2d1824d94a4cfd7f09979ff172e0bd4862ff8
Marián Hlaváč <hlavac95@gmail.com>
2017-01-14 19:57:49 +0100

    Change displayed metadata

--- app.js
+++ app.js
@@ -6,13 +6,17 @@ function updateUI() {
   question = store.questions[store.displayedQuestion]
 
   $('#question-id').html(store.displayedQuestion + 1)
+  $('#question-page').html(store.displayedQuestion + 1 + '/' + store.totalQuestions)
   $('#question-content').html(question.question.replace(/\n/g,'<br>'))
   $('#question-solution').html(question.solution.replace(/\n/g,'<br>'))
 
   $('#question-tags').html('')
-  question.tags.map(function(tag) {
-    $('#question-tags').append(`<span class="uk-badge">${tag}</span> `)
-  })
+
+  if ('tags' in question) {
+    question.tags.map(function(tag) {
+      $('#question-tags').append(`<span class="uk-badge">${tag}</span> `)
+    })
+  }
 
   if ('steps' in question) {
     $('#question-steps').html(question.steps.replace(/\n/g,'<br>'))
