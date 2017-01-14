a1832e9ba2c55991413897b68b6400fa09143b89
Marián Hlaváč <hlavac95@gmail.com>
2017-01-14 19:25:44 +0100

    Add question navigating

--- app.js
+++ app.js
@@ -11,12 +11,24 @@ function updateUI() {
 
   $('#question-tags').html('')
   question.tags.map(function(tag) {
-    $('#question-tags').append(`<span class="uk-badge">${tag}</span>`)
+    $('#question-tags').append(`<span class="uk-badge">${tag}</span> `)
   })
+
+  MathJax.Hub.Queue(["Typeset", MathJax.Hub]);
 }
 
 // ---
 
 $(document).ready(function() {
+  updateUI()
+})
+
+$('#prev-question-top, #prev-question-bot').click(function () {
+  store.displayedQuestion--
+  updateUI()
+})
+
+$('#next-question-top, #next-question-bot').click(function () {
+  store.displayedQuestion++
   updateUI()
 })
