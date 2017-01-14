37131a2934ece3658ccd4833cc76358338b745c5
Marián Hlaváč <hlavac95@gmail.com>
2017-01-14 20:18:11 +0100

    Add comments

--- app.js
+++ app.js
@@ -1,24 +1,31 @@
+/**
+ * App global state.
+ */
 var store = {
   displayedQuestion: 0
 };
 
+/**
+ * Updates the UI, propagating all state variables to the DOM.
+ */
 function updateUI() {
   question = store.questions[store.displayedQuestion]
 
+  // Update single-value always displayed fields
   $('#question-id').html(store.displayedQuestion + 1)
   $('#question-page').html(store.displayedQuestion + 1 + '/' + store.totalQuestions)
-
   $('#question-content').html(question.question.replace(/\n/g,'<br>'))
   $('#question-solution').html(question.solution.replace(/\n/g,'<br>'))
 
+  // Update tags
   $('#question-tags').html('')
-
   if ('tags' in question) {
     question.tags.map(function(tag) {
       $('#question-tags').append(`<span class='uk-badge'>${tag}</span> `)
     })
   }
 
+  // Update steps section or hide it
   if ('steps' in question) {
     $('#question-steps').html(question.steps.replace(/\n/g,'<br>'))
     $('#question-steps-tab').show()
@@ -26,13 +33,18 @@ function updateUI() {
     $('#question-steps-tab').hide()
   }
 
+  // Update source section or hide it
   if ('source' in question) {
     $('#question-source a').text(question.source[0]).attr('href', question.source[1])
   }
 
+  // Queue MathJax re-rendering
   MathJax.Hub.Queue(['Typeset', MathJax.Hub]);
 }
 
+/**
+ * Hides all solution and steps sections.
+ */
 function hideSolution() {
   $('#question-solution-tab').removeClass('uk-open')
   $('#question-solution').attr('aria-hidden', 'true').attr('hidden', 'hidden')
@@ -42,6 +54,9 @@ function hideSolution() {
 
 // ---
 
+/**
+ * Initialization after page load.
+ */
 $(document).ready(setTimeout(function() {
   updateUI()
 
@@ -50,6 +65,9 @@ $(document).ready(setTimeout(function() {
   })
 }, 500))
 
+/**
+ * Prev button click handling.
+ */
 $('#prev-question-top, #prev-question-bot').click(function () {
   if (store.displayedQuestion <= 0) return;
   store.displayedQuestion--
@@ -57,6 +75,9 @@ $('#prev-question-top, #prev-question-bot').click(function () {
   updateUI()
 })
 
+/**
+ * Next button click handling.
+ */
 $('#next-question-top, #next-question-bot').click(function () {
   if (store.displayedQuestion >= store.totalQuestions - 1) return;
   store.displayedQuestion++
@@ -64,6 +85,9 @@ $('#next-question-top, #next-question-bot').click(function () {
   updateUI()
 })
 
+/**
+ * Random button click handling.
+ */
 $('#rand-question').click(function () {
   store.displayedQuestion = Math.floor(Math.random() * store.totalQuestions)
   hideSolution()
